6a5202d777473d075f258b8b61ee30469809a638
Charly Riviere <charly.riviere@gmail.com>
2015-06-22 10:18:11 +0200

    Initial commit

---
+++ desktop/src/com/runProject/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.runProject.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.runProject.RunProject;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RunProject(), config);
+	}
+}
